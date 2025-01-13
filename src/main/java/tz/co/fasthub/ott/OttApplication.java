a5d8f4f52cf2e8bda77d354a69f044f37a02964f
Mnesa Ashku Hashim <mnesa.hashim@fasthub.co.tz>
2025-01-13 08:35:11 +0300

    Initial commit

---
+++ src/main/java/tz/co/fasthub/ott/OttApplication.java
@@ -0,0 +1,13 @@
+package tz.co.fasthub.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttApplication.class, args);
+	}
+
+}
